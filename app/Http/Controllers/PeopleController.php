10a41ca9cc4d13e03d42f8ae094b41315910da0c
Oleksandr Nykytin <keeper@ninydev.com>
2019-02-06 20:51:00 +0200

    blade

---
+++ app/Http/Controllers/PeopleController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Peoples;
+use Illuminate\Http\Request;
+
+class PeopleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        // return "Hello Peoples";
+        $data ["pageTitle"] = "Список особ в индустрии";
+        $data ["fromDB"] = Peoples::paginate(15);
+
+        return view('peoples/index',  $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\People  $people
+     * @return \Illuminate\Http\Response
+     */
+    public function show(People $people)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\People  $people
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(People $people)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\People  $people
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, People $people)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\People  $people
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(People $people)
+    {
+        //
+    }
+}
